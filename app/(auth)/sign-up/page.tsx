4f94491991945003ceeb86db640f559ff5703a64
Abraham <abrahamnhier97@gmail.com>
2024-08-07 10:10:26 +0300

    auth pages

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+        SignUp
+    </section>
+  )
+}
+
+export default SignUp
