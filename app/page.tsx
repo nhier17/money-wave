d84ecd2c191cf3dafd9699c2b55c6e4ed4d052d8
Abraham <abrahamnhier97@gmail.com>
2024-08-07 08:52:17 +0300

    initial setup

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+<section className="home">
+<div className="home-content">
+  <header className="home-header">
+    
+  </header>
+</div>
+</section>
+  );
+}
