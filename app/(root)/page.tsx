736bc264391fbff8b48437f4a1c66bad8c5991ab
Abraham <abrahamnhier97@gmail.com>
2024-08-07 10:26:09 +0300

    header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
+import HeaderBox from '@/components/HeaderBox'
 
 const Home = () => {
   return (
 <section className="home">
 <div className="home-content">
   <header className="home-header">
-    HeaderBox
+    <HeaderBox
+        type="greeting"
+        title="Welcome"
+        user='Guest'
+        subtext="Access and manage your account and transactions efficiently."
+    />
     TotalBalanceBox
   </header>
   RecentTransactions
